3e8301d8abde132175fe8dacb133d1d4b4e70104
William Lu <lu.william.m@gmail.com>
2015-02-19 23:30:59 -0800

    Second Commit
    Added reset button
    Rearranged Elements
    Only considers the latest 20 beats

--- app/src/main/java/wml/heartrate/MainActivity.java
+++ app/src/main/java/wml/heartrate/MainActivity.java
@@ -6,10 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
+
 import java.text.NumberFormat;
 import java.text.DecimalFormat;
 
@@ -19,8 +21,8 @@ import java.text.Format;
 public class MainActivity extends Activity {
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButton();
     }
@@ -48,65 +50,79 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    ImageButton imageButton;
-    double tapCounter;
-    long startTime;
-    long currentTime;
-    long previousTime;
+    ImageButton imageButton; Button resetButton;
+    //    double tapCounter;
+    int counter;
     boolean currentlyCounting;
     long[] times;
-        public void addListenerOnButton() {
 
-//         imageButton = (ImageButton) findViewById(R.id.HeartButton);
-//        imageButton.setOnClickListener();
+    public void addListenerOnButton() {
+
         imageButton = (ImageButton) findViewById(R.id.HeartButton);
-        tapCounter = 0;
+        resetButton = (Button) findViewById(R.id.resetButton);
+        final int beatsToConsider = 20;
+        times = new long[beatsToConsider];
+        currentlyCounting = false;
+        counter = 0;
 
-       currentlyCounting = false;
 
-        imageButton.setOnClickListener(new OnClickListener() {
+        resetButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                currentTime = System.currentTimeMillis();
-                TextView box = (TextView)findViewById(R.id.RateCounter);
-                long deltaTime;
-                if(!currentlyCounting)
-                {
-                    //just starting out
-                    startTime = currentTime;
-                    previousTime = startTime;
-                    currentlyCounting = true;
-                    tapCounter++;
-                    box.setText("Continue Tapping");
-                }
-                else
-                {
-                            deltaTime = currentTime - startTime;
-                            if((currentTime - previousTime)/1000.0 > 5)
-                            {
-                                //reset from overtime
-                                tapCounter = 0;
-                            currentlyCounting = false;
-
-                        box.setText("5 seconds have passed.\nCounter has reset");
-                    }
-                    else
-                    {
-                        tapCounter++;
-                        double heartRate = tapCounter/(deltaTime/60000.0);
-
-                        NumberFormat formatter = new DecimalFormat("##0.0");
-                        box.setText(formatter.format(heartRate) + " BPM");  previousTime = System.currentTimeMillis();
-                    }
-                }
-
-
-
-
+                TextView box = (TextView) findViewById(R.id.RateCounter);
+                counter = 0;
+                currentlyCounting = false;
+                box.setText("Tap Heart to Begin");
+                times = new long[beatsToConsider];
             }
 
         });
 
+
+        imageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                TextView box = (TextView) findViewById(R.id.RateCounter);
+                long deltaTime;
+                if (!currentlyCounting) {
+
+                        currentlyCounting = true;
+                        times[(counter % times.length)] = System.currentTimeMillis();
+                    counter++;
+                    box.setText("Continue Tapping");
+                } else {
+                    times[(counter % times.length)] = System.currentTimeMillis();
+                    if (counter >= times.length) {
+                        deltaTime = times[(counter % times.length)] - times[((counter + 1) % times.length)];
+                    } else {
+                        // array not full yet
+                        deltaTime = times[counter] - times[0];
+                    }
+
+                    if ((times[counter % times.length] - times[(counter - 1 ) % times.length]) > 5000) {// alternate code
+                        //reset from overtime
+                        counter = 0;
+                        currentlyCounting = false;
+                        box.setText("5 seconds have passed.\nCounter has reset");
+                        times = new long[times.length];
+                    }
+
+                    else {
+                        counter++;
+                        double heartRate = Math.min(counter, times.length) / (deltaTime/ 60000.0);
+                        NumberFormat formatter = new DecimalFormat("##0.0");
+                        box.setText(formatter.format(heartRate) + " BPM");
+//                        long x = (times[counter % times.length] - times[(counter - 1 ) % times.length]);
+////                        box.setText(formatter.format(heartRate) + " BPM\n" + " counter = " + counter +"\n deltaTime = " + deltaTime + " + \n" + x);
+//                        box.setText(x + " \n" + times[0] + "\n counter" + counter);
+
+                    }
+
+                }
+
+
+            }
+        });
     }
 
 }
